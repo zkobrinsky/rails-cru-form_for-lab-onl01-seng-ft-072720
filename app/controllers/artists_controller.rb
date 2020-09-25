022ad3d06baa9d6e13c813376b2fe8496efa14ee
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-25 11:56:18 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,50 @@
 class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+        current_artist
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+
+        if @artist.save
+            redirect_to @artist
+        else
+            flash[:alert] = @artist.errors.full_messages.join(". ")
+            redirect_to new_artist_path
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+
+
+    private
+
+        def current_artist
+            @artist = Artist.find_by(id: params[:id])
+        end
+
+        def artist_params
+            params.require(:artist).permit(:name, :bio)
+        end
 end
